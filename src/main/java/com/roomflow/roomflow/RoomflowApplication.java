a5c66763033aaec80ad68bd46892df4aaac95e82
kmsdevdata-sketch <kmsdevdata@gmail.com>
2026-03-03 17:51:25 +0900

    Project setting

---
+++ src/main/java/com/roomflow/roomflow/RoomflowApplication.java
@@ -0,0 +1,13 @@
+package com.roomflow.roomflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomflowApplication.class, args);
+	}
+
+}
