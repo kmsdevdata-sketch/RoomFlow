05b45e21eda8ce923dbf1634b508e812e8f7351e
kmsdevdata-sketch <kmsdevdata@gmail.com>
2026-03-12 14:49:19 +0900

    feat:UserCreateDto 도입

--- src/main/java/com/roomflow/web/controller/user/dto/UserCreateDto.java
+++ src/main/java/com/roomflow/web/controller/user/dto/UserCreateDto.java
@@ -1,5 +1,30 @@
 package com.roomflow.web.controller.user.dto;
 
+import jakarta.validation.constraints.*;
+import java.time.LocalDate;
+
 public class UserCreateDto {
+
+    @NotBlank
+    @Size(min = 4, max = 20)
+    private String loginId;
+
+    @NotBlank
+    @Size(min = 8, max = 20)
+    private String password;
+
+    @NotBlank
+    @Size(min = 2, max = 10)
+    private String name;
+
+    @Past
+    private LocalDate birthDate;
+
+    @NotBlank
+    @Pattern(regexp = "^01[0-9]-?\\d{3,4}-?\\d{4}$")
+    private String phoneNumber;
+
+    @Email
+    private String email;
 
 }
