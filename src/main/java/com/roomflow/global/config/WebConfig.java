f23709f9e6e9e5ff0cf3460b4cd37fe2a10e4259
kmsdevdata-sketch <kmsdevdata@gmail.com>
2026-03-19 18:58:12 +0900

    feat: LogFilter

---
+++ src/main/java/com/roomflow/global/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.roomflow.global.config;
+
+
+import com.roomflow.web.filter.LogFilter;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterRegistration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
